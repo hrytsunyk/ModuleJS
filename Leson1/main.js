f6c9333dc84fc5f402bcd5a5d848b357cad89a0c
hrytsunyk <hrytsunyk.hub@gmail.com>
2022-11-01 13:27:17 +0200

    done

---
+++ Leson1/main.js
@@ -0,0 +1,71 @@
+//
+let a = "Hello!";
+console.log(a);
+
+let b = "owu.";
+console.log(b);
+
+let c = "com.";
+console.log(c);
+
+let d = "ua";
+console.log(d);
+
+
+let num1 = 1;
+    console.log(num1);
+
+let num2 = 10;
+    console.log(num2);
+
+let num3= -999;
+    console.log(num3);
+
+let num4 = 123
+    console.log(num4);
+
+
+const PI = 3.14;
+console.log(PI);
+
+let num5 = 2.7;
+console.log(num5);
+
+let num6 = 16;
+console.log(num6);
+
+let e = true;
+console.log(9>8);
+
+let f = false;
+console.log(9<8);
+
+//
+let firstName = "Serhii";
+let middleName = "Oleksandrovych";
+let lastName = "Hrytsunyk";
+
+let person = (`${lastName} ${firstName} ${middleName}`);
+console.log(person);
+
+//
+let aa = 100;
+console.log(typeof aa)
+
+let bb = '100';
+console.log(typeof bb)
+
+let cc = true;
+console.log(typeof cc)
+
+//
+
+let info1 = prompt('Enter your name:');
+let info2 = prompt('Enter your middle name:');
+let info3 = prompt('Enter your last name:');
+
+console.log(info1, info2, info3);
+
+//
+
+
