3bfe1c6631d4847f4e5047e273b587c1122b501d
hrytsunyk <hrytsunyk.hub@gmail.com>
2022-11-08 16:54:43 +0200

    final edit

--- Lesson3/main.js
+++ Lesson3/main.js
@@ -17,17 +17,17 @@
 // - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
 // (в першу, другу, третю или четверту частину години).
 
-// let time = 11;
+// let time = 45;
 //
-// if (time <= 15) {
+// if (time >=0 && time <= 15) {
 //     console.log('Перша чверть години 1/4');
-// } else if (time <= 30) {
+// } else if (time >15 && time <= 30) {
 //     console.log('Друга чверть години 2/4');
 //
-// } else if (time <= 45) {
+// } else if (time >30 && time <= 45) {
 //     console.log('Третя чверть години 3/4');
 //
-// } else if (time <= 60) {
+// } else if (time >45 && time  <= 60) {
 //     console.log('Четверта чверть години 4/4');
 // } else {
 //     console.log('Try again')
@@ -37,44 +37,44 @@
 
 // - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
 
-// let day = +prompt('Уведи число місяця');
-// if (day <= 10) {
-//     console.log('Перша декада місяця');
-// } else if (day <= 20) {
-//     console.log('Друга декада місяця');
-// } else if (day <= 31) {
-//     console.log('Третя декада місяця');};
+let day = +prompt('Уведи число місяця');
+if (day >=1 && day <= 10) {
+    console.log('Перша декада місяця');
+} else if (day >=11 && day <= 20) {
+    console.log('Друга декада місяця');
+} else if (day >=21 && day <= 31) {
+    console.log('Третя декада місяця');};
 
 
 //
 // - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
 
-let schedule = +prompt('Уведи порядковий номер дня тижня');
-
- switch (schedule) {
-     case (1):
-         document.write('Sunday');
-         break;
-     case (2):
-         document.write('Monday');
-         break;
-     case (3):
-         document.write('Tuesday');
-         break;
-     case (4):
-         document.write('Wednesday');
-         break;
-     case (5):
-         document.write('Thursday');
-         break;
-     case (6):
-         document.write('Friday');
-         break;
-     case (7):
-         document.write('Saturday');
-         break;
-     default: document.write("????")
- };
+// let schedule = +prompt('Уведи порядковий номер дня тижня');
+//
+//  switch (schedule) {
+//      case (1):
+//          document.write('Sunday');
+//          break;
+//      case (2):
+//          document.write('Monday');
+//          break;
+//      case (3):
+//          document.write('Tuesday');
+//          break;
+//      case (4):
+//          document.write('Wednesday');
+//          break;
+//      case (5):
+//          document.write('Thursday');
+//          break;
+//      case (6):
+//          document.write('Friday');
+//          break;
+//      case (7):
+//          document.write('Saturday');
+//          break;
+//      default: document.write("????")
+//  };
 
 //
 //     - Користувач вводить або має два числа.
