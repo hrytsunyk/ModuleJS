57a73e32cf30e592abe45f80a18e9ebb1751206a
hrytsunyk <hrytsunyk.hub@gmail.com>
2022-11-23 01:22:10 +0200

    final work

---
+++ Lesson6/main.js
@@ -0,0 +1,181 @@
+// Знайти та вивести довижину настипних стрінгових значень
+//
+// let str =  'hello world, lorem ipsum, javascript is cool';
+//
+// console.log(str.length);
+//
+// - Перевести до великого регістру наступні стрінгові значення
+//
+// let str =  'hello world, lorem ipsum, javascript is cool';
+//
+// console.log(str.toUpperCase());
+//
+//
+// - Перевести до нижнього регістру настипні стрінгові значення
+//
+// let str = 'HELLO WORLD, LOREM IPSUM, JAVASCRIPT IS COOL';
+// console.log(str.toLowerCase());
+//
+//
+// - Є "брудна" стрінга . Почистити її від зайвих пробілів.
+//
+//
+// let str = ' dirty string   '
+// let trim = str.trim();
+// console.log(trim);
+//
+//
+//
+//
+// - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+// let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
+//
+//
+// let str = 'Ревуть воли як ясла повні';
+//
+// function stringToarray(str) {
+//     let newArr = [];
+//     newArr = str.split(' ')
+//     return newArr;
+// };
+// let result = stringToarray(str);
+// console.log(result);
+//
+//
+// - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
+//
+// let arrNumb = [10, 8, -7, 55, 987, -1011, 0, 1050, 0];
+//
+// let map = arrNumb.map(value => value + '')
+// console.log(map)
+//
+// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+// sortNums(nums,'ascending') // [3,11,21]
+// sortNums(nums,'descending') // [21,11,3]
+//
+//
+// let nums = [11, 21, 3];
+//
+//
+// function sortNums(arr, direction) {
+//     let sortedNums = [];
+//     if (direction === 'ascending') {
+//         arr.sort((a, b) => a - b);
+//         sortedNums.push(arr);
+//     } else if (direction === 'descending') {
+//         arr.sort((a, b) => b - a);
+//         sortedNums.push(arr);
+//     }
+//     return sortedNums;
+// };
+//
+// let result1 = sortNums(nums, 'descending');
+// console.log(result1)
+//
+//
+//
+// - є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+//
+//
+// -- відсортувати його за спаданням за monthDuration
+// let sortDesc= coursesAndDurationArray.sort((a,b) => {
+//      return (b.monthDuration - a.monthDuration)
+// });
+// console.log(sortDesc);
+//
+//
+// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+// let filter = coursesAndDurationArray.filter((a) => {
+//     return (a.monthDuration > 5);
+// });
+//
+// console.log(filter);
+//
+//
+//
+// описати колоду карт
+// - знайти піковий туз
+// - всі шістки
+// - всі червоні карти
+// - всі буби
+// - всі трефи від 9 та більше
+//
+//
+// let cardSuit = ['spade', 'diamond', 'heart', 'clubs'];
+// let value = ['6','7','8','9','10', 'ace', 'jack', 'queen', 'king'];
+// let color = ['red', 'black'];
+//
+// let cards = [
+//     {cardSuite: 'spade', color: 'black'},
+//     {cardSuite: 'diamond', color: 'red'},
+//     {cardSuite: 'heart', color: 'red'},
+//     {cardSuite: 'clubs', color: 'black'}
+// ];
+//
+// let values = [6, 7, 8, 9, 10, 'ace', 'jack', 'queen', 'king']
+//
+// let deck = [];
+//
+// for (const card of cards) {
+//     for (const priority of values) {
+//         let sorted = {cardSuite: card.cardSuite, color: card.color, value: priority}
+//         deck.push(sorted)
+//     }
+//
+// }
+//
+// console.log(deck)
+//
+// let filtered = [];
+//
+// let filter1 = deck.filter((value) => value.cardSuite === 'spade' && value.value === 'ace');
+// filtered.push(filter1);
+//
+// let filter2 = deck.filter((value) => value.value === 6);
+// filtered.push(filter2)
+//
+// let filter3 = deck.filter((value) => value.color === 'red');
+// filtered.push(filter3);
+//
+// let filter4 = deck.filter((value) => value.cardSuite === 'diamond')
+// filtered.push(filter4)
+//
+// let filter5 = deck.filter((value => (value.cardSuite === 'clubs' && value.value >= 9) ||
+//     (value.cardSuite === 'clubs' && typeof value.value === 'string')))
+// filtered.push(filter5)
+//
+// console.log(filtered)
+//
+//
+//
+// Додатково по reduce
+// Взяти описану колоду карт, та за допомоги редюсу попакувати всі карти по "мастях" в об'єкт
+//
+// let reduce = deck.reduce((accum, value) => {
+//     if (value.cardSuite === 'spade') {
+//         accum.spades.push(value)
+//     } else if (value.cardSuite === 'diamond') {
+//         accum.diamonds.push(value)
+//     } else if (value.cardSuite === 'heart') {
+//         accum.hearts.push(value)
+//     } else if (value.cardSuite === 'clubs') {
+//         accum.clubs.push(value)
+//     }
+//     return accum;
+// }, {
+//     spades: [],
+//     diamonds: [],
+//     hearts: [],
+//     clubs: []
+// })
+//
+//
+// console.log(reduce);
--- additional/Demo4/main.js
+++ additional/Demo4/main.js
@@ -54,9 +54,9 @@
 // let maxNumb = (arr) => {
 //     let value = 0;
 //     for (const  item of arr) {
-//             value+= item / arr.length;
+//             value+= item;
 //         }
-//     return value;
+//     return value / arr.length;
 //     }
 //
 // console.log(maxNumb(array));
@@ -65,38 +65,139 @@
 
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
 
-// let nNumber = function () {
-//     console.log(arguments);
+// let nNumber = function (...arguments) {
+// let min = arguments [0];
+// let max = arguments [0];
 //
-//     if (arguments.length < 0) {
-//       return arguments;
-//
-//     } else if (arguments.length > 0) {
-//
+//     for (const item of arguments) {
+//         if (item< min) {
+//             min = item;
+//         } if (item > max) {
+//         max=item;
+//         }
 //     }
-// }
-// nNumber(1, 54, 5, 57, 26, 5, 767)
+//     console.log(max);
+//     return min;
+//     }
+// console.log(nNumber(1, 54, 5, 57, 26, 5, 767));
 
 // - створити функцію яка заповнює масив рандомними числам
 // (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
+// let arr = [];
+//
+// function foo (array) {
+//     for (let i = 0; i < 100; i++) {
+//         arr [i] = Math.round(Math.random() * 100);
+//     }
+//     console.log(arr);
+//
+// }
+// foo ()
+
+
 // - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+
+// let arr= [];
+//
+// function foo(limit) {
+//     for (let i = 0; i < limit; i++) {
+//         arr [i] = Math.round(Math.random()*100)
+//     }
+//     console.log(arr);
+// }
+// foo (15)
+
+
 // - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+
+// let num = [2, 5, 64, 23, 5, 62, 5, 3];
 //
+// function foo(array) {
+//     let mun = [];
+//     for (let i = array.length - 1, j = 0; i >= 0; i--) {
+//         mun [j++] = num [i];
+//     }
+//     return mun;
+// }
 //
+// console.log(foo(num));
+
+
 //
 //
 // - створити функцію, яка якщо приймає один аргумент, просто вивдоить його, якщо два аргументи - складає або конкатенує їх між собою.
+
+// let get = (...arguments) => arguments.length===2 ? arguments [0] + arguments [1]: arguments [0];
+//
+// let res= get(3,'3');
+// console.log(res);
+
 //
 // - створити функцію  яка приймає два масиви та скаладає значення елементів з однаковими індексами  та повертає новий результуючий масив.
 //     EXAMPLE:
-// [1,2,3,4]
-//     [2,3,4,5]
+// let arr = [1, 2, 3, 4];
+// let arr2 = [2, 3, 4, 5, 5];
+//
+// function foo(array1, array2) {
+//     let res = [];
+//     let por = array1.length >= array2.length ? array1 : array2;
+//     for (let i = 0; i < por.length; i++) {
+//         res [i] = (array1 [i] || 0) + (array2 [i] || 0);
+//     }
+//     return res;
+// }
+//
+// console.log(foo(arr, arr2));
+
 // результат
 //     [3,5,7,9]
 //
 // - Створити функцію яка приймає масив будь яких объектів, та повертає масив ключів всіх обєктів
 // EXAMPLE:
 //     [{name: 'Dima', age: 13}, {model: 'Camry'}]  ===> [ name, age, model ]
+
+let objects = [
+    {name: 'Dima', age: 13},
+    {model: 'Camry'}];
+//
+// function foo(arr) {
+//     let res = [];
+//
+//     for (let i = 0, j = 0; i <  arr.length; i++) {
+//         for (const key in arr[i]) {
+//            res [j++] = key;
+//        }
+//
+//     }
+//         return res;
+// }
+
+// console.log(foo(objects));
+//
+// function foo(array) {
+//     let newarr = [];
+//     for (let i = 0, j=0; i < array.length; i++) {
+//         for (const key in array [i]) {
+//             newarr [j++] = key;
+//         }
+//
+//     }
+//     return newarr;
+// }
+//
+// console.log(foo(objects));
+
+//
+// function ofo(array) {
+//     let arr = [];
+//     for (let i = 0, j = 0; i < array.length; i++) {
+//
+//     }
+//     return arr;
+// }
+//
+// console.log(ofo(objects));
+
 //
 //     - Створити функцію яка приймає масив будь яких объектів, та повертає масив значень всіх обєктів
 // EXAMPLE:
