8ca5bacc8e7ae37e52fcaba0e97e8c7db41b7ab3
hrytsunyk <hrytsunyk.hub@gmail.com>
2022-12-06 02:55:16 +0200

    cinderella

--- additional/demo6/main.js
+++ additional/demo6/main.js
@@ -28,14 +28,14 @@ users.push(user1, user2, user3, user4, user5, user6, user7, user8, user9, user10
 console.log(users)
 // - Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
 
-let filter = users.filter(value => value.id %2===0 ? value.id : null);
+let filter = users.filter(value => value.id % 2 === 0 ? value.id : null);
 console.log(filter);
 //
 //
 ///
 // - Взяти масив з  User[] з попереднього завдання, та відсортувати його по id. по зростанню (sort)
 
-let sort = users.sort((a,b) => a.id - b.id)
+let sort = users.sort((a, b) => a.id - b.id)
 console.log(sort);
 
 
@@ -44,14 +44,14 @@ console.log(sort);
 // створити пустий масив, наповнити його 10 об'єктами Client
 
 class Client {
-constructor(id, name, surname, email, phone, orders) {
-    this.name = name;
-    this.id = id;
-    this.surname = surname;
-    this.email = email;
-    this.phone = phone;
-    this.orders = orders;
-}
+    constructor(id, name, surname, email, phone, orders) {
+        this.name = name;
+        this.id = id;
+        this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+        this.orders = orders;
+    }
 };
 let clients = [];
 
@@ -66,44 +66,17 @@ let client8 = new Client(8, 'Mary', 'Marrisson', 'marisson@gmail.com', +38000000
 let client9 = new Client(9, 'Steven', 'Stevenson', 'stevenson@gmail.com', +380000000009, ['eggs', 'water', 'cheese', 'tea', 'milk', 'meat']);
 let client10 = new Client(10, 'Jane', 'Janesson', 'janesson@gmail.com', +380000000010, ['eggs', 'water']);
 
-clients.push(client1,client2,client3,client4,client5,client6,client7,client8,client9,client10);
+clients.push(client1, client2, client3, client4, client5, client6, client7, client8, client9, client10);
 console.log(clients);
 
 
 // - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
 //
-let sortClients = clients.sort((a,b) => a.orders.length - b.orders.length)
+let sortClients = clients.sort((a, b) => a.orders.length - b.orders.length)
 console.log(sortClients);
 //
 // - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
 
-class SalonCars {
-    constructor(car, model, manufactured, year, maxSpeed, engineCapacity) {
-        this.car = car;
-        this.model = model;
-        this.manufactured = manufactured;
-        this.year = year;
-        this.maxSpeed = maxSpeed;
-        this.engineCapacity = engineCapacity;
-    }
-    getDrive () {
-        console.log(`Їдемо зі швидкістю ${this.maxSpeed}км/годину!`);
-    }
-    getIngo () {
-        console.log(`Інформація: 
-        ${this.car}  
-        ${this.model} 
-        ${this.manufactured} 
-        ${this.year} 
-        ${this.maxSpeed} 
-        ${this.engineCapacity}`)
-    }
-
-};
-
-let car1 = new SalonCars('Mitsubishi', 'Colt', 'Japon', 2006, 120, 1.3)
-
-car1.getIngo()
 
 
 // -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
@@ -123,21 +96,106 @@ car1.getIngo()
 //
 //
 //
+class SalonCars {
+    constructor(car, model, manufactured, year, maxSpeed, engineCapacity, name) {
+        this.car = car;
+        this.model = model;
+        this.manufactured = manufactured;
+        this.year = year;
+        this.maxSpeed = maxSpeed;
+        this.engineCapacity = engineCapacity;
+        this.name = name;
+    }
+
+    drive() {
+        console.log(`Їдемо зі швидкістю ${this.maxSpeed}км/годину!`);
+    }
+
+    get info() {
+        console.log(this);
+    }
+
+    set increaseMaxSpeed(newSpeed) {
+        this.maxSpeed = newSpeed;
+    }
+
+    set changeYear(newValue) {
+        this.year = newValue;
+    }
+
+    set addDriver(driver) {
+        this.name = driver
+    }
+
+};
+
+let car1 = new SalonCars('Mitsubishi', 'Colt', 'Japan', 2006, 120, 1.3,);
+let car2 = new SalonCars('Mitsubishi', 'Pajero', 'Japan', 2020, 280, 2.5,);
+let car3 = new SalonCars('Volkswagen', 'Tuareg', 'Germany', 2021, 270, 3);
+let car4 = new SalonCars('Volkswagen', 'Caddy', 'Germany', 2009, 150, 1.9);
+let car5 = new SalonCars('Ford', 'Fusion', 'USA', 2022, 200, 2);
+let car6 = new SalonCars('Ford', 'EDGE', 'USA', 2019, 275, 2.5);
+let car7 = new SalonCars('Ford', 'Mustang', 'USA', 2017, 340, 3.5);
+let car8 = new SalonCars('Fiat', '500', 'Italy', 2008, 150, 1.8);
+let car9 = new SalonCars('Fiat', 'Scudo', 'Italy', 2009, 130, 2.5);
+let car10 = new SalonCars('Toyota', 'Camry', 'Japan', 2020, 280, 2.8);
+
+let cars = [car1, car2, car3, car4, car5, car6, car7, car8, car9, car10];
+console.log(cars);
+
+car1.drive();
+car1.info;
+car1.increaseMaxSpeed = 80;
+console.log(car1)
+car1.changeYear = 2001;
+console.log(car1);
+car1.addDriver = 'Mr. Green';
+console.log(car1)
+//
+//
+///
+///
+//
 // -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
 // Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
 //     За допомоги циклу знайти яка попелюшка повинна бути з принцом.
 //     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
-// Footer
-// © 2022 GitHub, Inc.
-//     Footer navigation
-// Terms
-// Privacy
-// Security
-// Status
-// Docs
-// Contact GitHub
-// Pricing
-// API
-// Training
-// Blog
-// About
+
+function CinderellaPrince(name, age, footSize) {
+    this.name = name;
+    this.age = age;
+    this.footSize = footSize;
+}
+
+let cinderella1 = new CinderellaPrince('Cinderella-1', 22, 38);
+let cinderella2 = new CinderellaPrince('Cinderella-2', 23, 39);
+let cinderella3 = new CinderellaPrince('Cinderella-3', 24, 40);
+let cinderella4 = new CinderellaPrince('Cinderella-4', 25, 45);
+let cinderella5 = new CinderellaPrince('Cinderella-5', 26, 39);
+let cinderella6 = new CinderellaPrince('Cinderella-6', 27, 32);
+let cinderella7 = new CinderellaPrince('Cinderella-7', 28, 35);
+let cinderella8 = new CinderellaPrince('Cinderella-8', 29, 36);
+let cinderella9 = new CinderellaPrince('Cinderella-9', 20, 37);
+let cinderella10 = new CinderellaPrince('Cinderella-10', 24, 43);
+
+
+class Prince {
+    constructor(name, age, shoe) {
+        this.name = name;
+        this.age = age;
+        this.shoe = shoe;
+    }
+};
+
+let prince = new Prince('Prince', 30, 37);
+let cinderellas = [cinderella1, cinderella2, cinderella3, cinderella4, cinderella5, cinderella6, cinderella7, cinderella8, cinderella9, cinderella10, prince];
+
+for (let i = 0; i < cinderellas.length; i++) {
+    const cinderella = cinderellas[i];
+    let result = (cinderella.footSize === prince.shoe) ? console.log(cinderella.name) : null;
+
+}
+
+let find = cinderellas.find(value => value.name === 'Cinderella-9');
+console.log(find);
+
